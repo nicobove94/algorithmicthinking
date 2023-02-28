9e322d9107ff07825d9195b9ed7e1dbc93078460
nicobove <nicolasbove7@gmail.com>
2023-02-28 12:01:01 -0500

    added oaat hashing function and header file

--- hashtables/compoundwords/compoundwords.c
+++ hashtables/compoundwords/compoundwords.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "oaat.h"
 #define NUM_BITS 17
 #define WORD_LENGTH 16
 
---
+++ hashtables/compoundwords/oaat.h
@@ -0,0 +1,6 @@
+#ifndef OAAT_H
+#define OAAT_H
+
+unsigned int oaat(char *key, unsigned long len, unsigned long bits);
+
+#endif /* OAAT_H */
